95713fd43e39d3dd5a9e392c83f982161898a1f0
jinoc <choiae369@gmail.com>
2023-05-16 17:04:33 +0900

    게시글 불러오기, 보내기, 삭제 가능

--- app.py
+++ app.py
@@ -13,15 +13,19 @@ def home():
     return render_template('index.html')
 
 #   게시글 작성
-@app.route("/introduce", methods=["POST"])
+@app.route("/introduce/upload", methods=["POST"])
 def info_post():
     name_receive = request.form['name_give']
     img_receive = request.form['img_give']
     comment_receive = request.form['comment_give']
+    mbti_receive = request.form['mbti_give']
+    blog_receive = request.form['blog_give']
 
     doc = {
         'name' : name_receive,
         'img' : img_receive,
+        'mbti' : mbti_receive,
+        'blog' : blog_receive,
         'comment' : comment_receive
     }
 
@@ -35,25 +39,37 @@ def modify_info():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
     img_receive = request.form['img_give']
+    mbti_receive = request.form['mbti_give']
+    blog_receive = request.form['blog_give']
     id_receive = request.form['_id_give']
     
     doc = {
         'name' : name_receive,
         'comment' : comment_receive,
+        'mbti' : mbti_receive,
+        'blog' : blog_receive,
         'img' : img_receive
     }
-    print("확인", id_receive, doc)
+    # print("확인", id_receive, doc)
 
     db.introduce.update_one({"_id": ObjectId(id_receive)}, {"$set" : doc})
 
     return jsonify({'msg':'게시글 수정 완료!'})
 
+#   게시글 삭제
+@app.route("/introduce/delete", methods=["POST"])
+def del_info():
+    id_receive = request.form['_id_give']
+
+    db.introduce.delete_one({"_id": ObjectId(id_receive)})
+
+    return jsonify({'msg':'게시글 삭제 완료!'})
 
 
 #   게시글 불러오기
 from bson.objectid import ObjectId
 
-@app.route("/introduce", methods=["GET"])
+@app.route("/introduce/read", methods=["GET"])
 def info_get():
     all_info = list(db.introduce.find({}))
     
